a8e2c68aa46ad7e347fdbf9527b5bd288e820284
Shawn Eion Smith <ses44@psu.edu>
2016-05-17 11:10:00 -0400

    Adding swagger

--- eduperson-scim-web/src/main/java/edu/psu/swe/eduperson/rest/MyApplication.java
+++ eduperson-scim-web/src/main/java/edu/psu/swe/eduperson/rest/MyApplication.java
@@ -6,25 +6,33 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import edu.psu.swagger.util.SwaggerHelper;
+import edu.psu.swagger.util.SwaggerJaxrsConfig;
 import edu.psu.swe.scim.server.rest.ScimResourceHelper;
 
 @ApplicationPath("v2")
 public class MyApplication extends Application {
-  
- @Override
- public Set<Class<?>> getClasses() {
-   Set<Class<?>> clazzes = new HashSet<Class<?>>();
-   
-   clazzes.addAll(ScimResourceHelper.getScimClassesToLoad());
-   clazzes.add(EduPersonRestResource.class);
-   return clazzes;
- }
 
-// @Override
-// public Set<Object> getSingletons()
-// {
-//   Set<Object> singletons = new HashSet<>();
-//   singletons.add(new VersionResource(this.getClass()));
-//   return singletons;
-// }
+  @Override
+  public Set<Class<?>> getClasses() {
+    Set<Class<?>> clazzes = new HashSet<Class<?>>();
+
+    clazzes.addAll(ScimResourceHelper.getScimClassesToLoad());
+    clazzes.add(EduPersonRestResource.class);
+    clazzes.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+    clazzes.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+    
+    clazzes.addAll(SwaggerHelper.getSwaggerClassesToLoad());
+    SwaggerJaxrsConfig.beanConfig.setTitle("EduPerson SCIM");
+
+    return clazzes;
+  }
+
+  // @Override
+  // public Set<Object> getSingletons()
+  // {
+  // Set<Object> singletons = new HashSet<>();
+  // singletons.add(new VersionResource(this.getClass()));
+  // return singletons;
+  // }
 }
