a63f7c61851b6221dadd92f0d84fb5aa1e8891e8
Christopher Harm <crh5255@psu.edu>
2016-05-19 12:25:38 -0400

    Adding ResourceType swagger documentations

--- eduperson-scim-web/src/main/java/edu/psu/swe/eduperson/rest/EduPersonRestResource.java
+++ eduperson-scim-web/src/main/java/edu/psu/swe/eduperson/rest/EduPersonRestResource.java
@@ -1,5 +1,7 @@
 package edu.psu.swe.eduperson.rest;
 
+import io.swagger.annotations.Api;
+
 import javax.inject.Inject;
 import javax.ws.rs.Path;
 
@@ -9,6 +11,7 @@ import edu.psu.swe.scim.server.provider.ProviderRegistry;
 import edu.psu.swe.scim.server.rest.BaseResourceTypeResourceImpl;
 
 @Path("EduPeople")
+@Api("ResourceType")
 public class EduPersonRestResource extends BaseResourceTypeResourceImpl<EduPersonResource> {
   
   @Inject
